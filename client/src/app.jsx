78d05b52b001b8fed2ed8c358e9a22c7636e5307
David Hong <davydhong@gmail.com>
2018-09-12 10:43:27 -0700

    webpack-hot-middleware working

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
-import App from './app.jsx';
 
-ReactDOM.render(<App />, document.getElementById('HEADER'));
+const App = () => <div>Hello World8!</div>;
+
+export default hot(module)(App);
---
+++ server/spec/index.js
@@ -0,0 +1,1 @@
+const jest = require('jest');
