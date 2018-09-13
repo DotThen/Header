966d4cfd0afd36a223db7bfab74e303847bae5c4
David Hong <davydhong@gmail.com>
2018-09-12 20:22:06 -0700

    moving to testing

--- server/spec/index.test.js
+++ server/spec/index.test.js
@@ -1,9 +1,23 @@
-test('two plus two is four', () => {
-  expect(2 + 5).toBe(7);
+const request = require('supertest');
+const app = require('../index.js');
+
+describe('Test the root path', () => {
+  test('It should response the GET method', done => {
+    request(app)
+      .get('/artists/1')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
 });
 
-test('object assignment', () => {
-  const data = { one: 1 };
-  data['two'] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
-});
+// test('two plus two is four', () => {
+//   expect(2 + 5).toBe(7);
+// });
+
+// test('object assignment', () => {
+//   const data = { one: 1 };
+//   data['two'] = 2;
+//   expect(data).toEqual({ one: 1, two: 2 });
+// });
