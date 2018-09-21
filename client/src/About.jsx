92048b040e21db851d21eba97afe1f4dc7566035
David Hong <davydhong@gmail.com>
2018-09-21 14:31:16 -0700

    nav bar and expanding transition working

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -5,10 +5,22 @@ import AboutStyle from './styles/About.css';
 import classNames from 'classnames';
 import ArtistPics from './ArtistPics.jsx';
 import LocationStat from './LocationStat.jsx';
+import headerStyle from './styles/header.css';
 
+const CollapseBtnClass = classNames(headerStyle.textButton, headerStyle.follow, 'button', 'biography-toggle-collapse');
 class About extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      collapseBio: true
+    };
+    this.handleCollapse = this.handleCollapse.bind(this);
+  }
+
+  handleCollapse() {
+    this.setState(state => ({
+      collapseBio: !state.collapseBio
+    }));
   }
   render() {
     console.log('COMPONENT: ABOUT:', this.props);
@@ -18,11 +30,16 @@ class About extends Component {
         <div className="about-container-left">
           <div className="biography-container">
             <h2 className="biography-header">Bio</h2>
-            {this.props.artist.about.Biography.split('\n').map(paragraph => (
-              <React.Fragment>
-                <p className="biography-txt">{paragraph}</p>
-              </React.Fragment>
-            ))}
+            <div className={`biography-txt-container ${this.state.collapseBio ? 'collapsed' : 'expanded'}`}>
+              {this.props.artist.about.Biography.split('\n').map(paragraph => (
+                <React.Fragment>
+                  <p className="biography-txt">{paragraph}</p>
+                </React.Fragment>
+              ))}
+            </div>
+            <button className={CollapseBtnClass} onClick={this.handleCollapse}>
+              {this.state.collapseBio ? 'EXPAND' : 'HIDE'}
+            </button>
           </div>
           <ArtistPics artistImages={this.props.artist.artistImages} />
         </div>
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -8,7 +8,51 @@ import searchIcon from './icons/search-icon.svg';
 class NavBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchOn: false,
+      homeOn: false,
+      libraryOn: false
+    };
+    this.handleReset = this.handleReset.bind(this);
+    this.handleToggleSearch = this.handleToggleSearch.bind(this);
+    this.handleToggleHome = this.handleToggleHome.bind(this);
+    this.handleToggleLibrary = this.handleToggleLibrary.bind(this);
   }
+  handleReset() {
+    this.setState(state => ({
+      searchOn: false,
+      homeOn: false,
+      libraryOn: false
+    }));
+  }
+  handleToggleSearch() {
+    if (!this.state.searchOn) {
+      this.setState(state => ({
+        searchOn: true,
+        homeOn: false,
+        libraryOn: false
+      }));
+    }
+  }
+  handleToggleHome() {
+    if (!this.state.homeOn) {
+      this.setState(state => ({
+        searchOn: false,
+        homeOn: true,
+        libraryOn: false
+      }));
+    }
+  }
+  handleToggleLibrary() {
+    if (!this.state.libraryOn) {
+      this.setState(state => ({
+        searchOn: false,
+        homeOn: false,
+        libraryOn: true
+      }));
+    }
+  }
+
   render() {
     console.log('COMPONENT:NAVBAR', typeof spotifyLogo);
     return (
@@ -16,7 +60,7 @@ class NavBar extends Component {
         <div className="nav-bar-container">
           <nav className="navBar">
             <div className="navBar-expand">
-              <div className="navBar-header">
+              <div className="navBar-header" onClick={this.handleReset}>
                 <i
                   dangerouslySetInnerHTML={{
                     __html: require('./icons/spotify-logo--text.svg').toString()
@@ -24,36 +68,66 @@ class NavBar extends Component {
                   className="logo"
                 />
               </div>
-              <br/><br/>
-              <div className="navBar-item">
-                <i
-                  dangerouslySetInnerHTML={{
-                    __html: require('./icons/search-icon.svg').toString()
-                  }}
-                  className="icon search-icon"
-                />
-                <span className="navbar-link__text">  Search</span>
-              </div>
-              <br/>
-              <div className="navBar-item">
-                <i
-                  dangerouslySetInnerHTML={{
-                    __html: require('./icons/home-icon.svg').toString()
-                  }}
-                  className="icon home-icon"
-                />
-                <span className="navbar-link__text">  Home</span>
-              </div>
-              <br/>
-              <div>
-                <div className="navBar-item">
-                  <span
+              <br />
+              <br />
+              <div className={`navBar-item search ${this.state.searchOn ? 'active' : 'default'}`} onClick={this.handleToggleSearch}>
+                {this.state.searchOn ? (
+                  <i
                     dangerouslySetInnerHTML={{
-                      __html: require('./icons/collection-icon.svg').toString()
+                      __html: require('./icons/search-active-icon.svg').toString()
                     }}
-                    className="icon collection-icon"
+                    className="icon search-active-icon"
                   />
-                  <span className="navbar-link__text">  Your Library</span>
+                ) : (
+                  <i
+                    dangerouslySetInnerHTML={{
+                      __html: require('./icons/search-icon.svg').toString()
+                    }}
+                    className="icon search-icon"
+                  />
+                )}
+                <span className="navbar-link__text" style={{ background: 'green !important' }}>
+                  Search
+                </span>
+              </div>
+              <br />
+              <div className={`navBar-item home ${this.state.homeOn ? 'active' : 'default'}`} onClick={this.handleToggleHome}>
+                {this.state.homeOn ? (
+                  <i
+                    dangerouslySetInnerHTML={{
+                      __html: require('./icons/home-active-icon.svg').toString()
+                    }}
+                    className="icon home-active-icon"
+                  />
+                ) : (
+                  <i
+                    dangerouslySetInnerHTML={{
+                      __html: require('./icons/home-icon.svg').toString()
+                    }}
+                    className="icon home-icon"
+                  />
+                )}
+                <span className="navbar-link__text"> Home</span>
+              </div>
+              <br />
+              <div>
+                <div className={`navBar-item library ${this.state.libraryOn ? 'active' : 'default'}`} onClick={this.handleToggleLibrary}>
+                  {this.state.libraryOn ? (
+                    <i
+                      dangerouslySetInnerHTML={{
+                        __html: require('./icons/collection-active-icon.svg').toString()
+                      }}
+                      className="icon collection-active-icon"
+                    />
+                  ) : (
+                    <i
+                      dangerouslySetInnerHTML={{
+                        __html: require('./icons/collection-icon.svg').toString()
+                      }}
+                      className="icon collection-icon"
+                    />
+                  )}
+                  <span className="navbar-link__text"> Your Library</span>
                 </div>
               </div>
             </div>
